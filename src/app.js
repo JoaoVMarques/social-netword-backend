70ae6ed9e553507cfd7cc0150f4f2c178224702a
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-10-17 18:42:03 -0300

    Adicionando configurações para o header do server para outros sites fazerem requisição

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const config = require('./config');
 const { postsRouter } = require('./routers');
 
 const app = express();
 
+app.use(config);
 app.use('/posts', postsRouter);
 
 module.exports = app;
